40d68c0b18d62651c54f13eb8ed8bc4a55bf88ad
naufalajaa <naufalajaa475@gmail.com>
2023-04-12 23:08:14 +0700

    menanbahkan tanggal pengerjaan,nim,nama,kelas

--- app/src/main/java/com/example/a10120246latihan1/MainActivity.kt
+++ app/src/main/java/com/example/a10120246latihan1/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.a10120246latihan1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Tnggal pengerjaan : 10 April 2023
+//NIM               : 10120246
+//Nama              : Muhammad Naufal Malikulmulki
+//Kelas             : IF-6
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
